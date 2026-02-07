dd44032eecec983c9161d996af6604e8172982c9
wndus0212 <beny020101@gmail.com>
2026-02-07 20:54:54 +0900

    fix: name fix

--- frontend/src/stores/useDataStore.ts
+++ frontend/src/stores/useDataStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
-import localClusterData from '../data/clustered_Data.json';
+import localClusterData from '../data/clustered_data.json';
 
 interface audioFeatures {
     energy: number;
